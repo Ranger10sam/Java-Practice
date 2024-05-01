a77738c2f5637f1e1801ccb6b3590030cf00c6cd
Ranger10sam <sampritchaudhury@gmail.com>
2024-05-01 23:59:20 +0530

    Added VotingVerification Folder

---
+++ VotingVerification/votingVerification.java
@@ -0,0 +1,20 @@
+package VotingVerification;
+
+import java.util.*;
+
+public class votingVerification {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        int a = sc.nextInt();
+
+        System.out.println("Enter your age: ");
+
+
+        if(a>=18){
+            System.out.println("Eligible!!!");
+        }else{
+            System.out.println("Not Eligible!!!");
+        }
+    }
+}
