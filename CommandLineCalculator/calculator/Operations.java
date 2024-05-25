17c57ef3a21f5f7e47f4944ba7d027474a990f05
Ranger10sam <sampritchaudhury@gmail.com>
2024-05-25 22:52:08 +0530

    added App.java V1

---
+++ CommandLineCalculator/calculator/Operations.java
@@ -0,0 +1,18 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class Operations {
+    long Addition(long a , long b){
+        return a+b;
+    }
+    long Subtraction(long a , long b){
+        return a-b;
+    }
+    long Multiplication(long a , long b){
+        return a*b;
+    }
+    long Division(long a , long b) {
+        return a / b;
+    }
+}
